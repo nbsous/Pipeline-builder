c25c5642ccfbdfada774f141d8d2c3f01788f296
Alexander Rowe <adrowe1@gmail.com>
2017-09-20 21:51:13 +0200

    Simple script updates

--- R-scripts/findNewBams.R
+++ R-scripts/findNewBams.R
@@ -1,6 +1,45 @@
 # Identify all bam files present in bam_repo, check whether already present in SQLite database, if not, add.
+library(magrittr)
+
 paths <- commandArgs(trailingOnly = TRUE)
 bam_repo <- as.character(paths[1])
 db_path <- as.character(paths[2])
 
-system(paste("echo \"", bam_repo, db_path, "\" > tmp.txt"))
+# system(paste("echo \"", bam_repo, db_path, "\" > tmp.txt"))
+
+# Connect SQLite DB 
+logging_db <- DBI::dbConnect(RSQLite::SQLite(), dbname = db_path)
+
+# get all bam files in repo
+all_bams_in_repo <- bam_repo %>% list.files(pattern=".bam$")
+# get all bai files in repo
+all_bais_in_repo <- bam_repo %>% list.files(pattern=".bai$")
+# get all vcf files in repo
+all_vcfs_in_repo <- bam_repo %>% list.files(pattern=".vcf$")
+
+# get all bam files in db
+all_bams_in_db <- DBI::dbGetQuery(logging_db, "SELECT BamFilename FROM InputFiles") %>% unlist()
+
+# all new bams not in db
+all_new_bams_in_repo <- setdiff(all_bams_in_repo, all_bams_in_db)
+
+if (length(all_new_bams_in_repo) > 0) {
+  # bai file exists
+  bai_file_exists <- match(all_new_bams_in_repo, all_bais_in_repo %>% stringr::str_replace(".bai", "")) %>% is.na() %>% not()
+  # vcf file exists
+  vcf_file_exists <- match(all_new_bams_in_repo, all_bais_in_repo %>% stringr::str_replace(".vcf", "")) %>% is.na() %>% not()
+  
+  
+  # Define new bam table
+  new_bam_table <- data.frame(BamFilename=all_new_bams_in_repo, 
+                              HasBaiFile=bai_file_exists, 
+                              HasVcfFile=vcf_file_exists, 
+                              ProcessStatus="Queued")
+  
+  # append newly detected bam files to table in DB
+  DBI::dbWriteTable(logging_db, "InputFiles", new_bam_table, append = TRUE)
+  
+}
+
+# Disconnect DB
+DBI::dbDisconnect(logging_db)
--- R-scripts/initSQLiteDB.R
+++ R-scripts/initSQLiteDB.R
@@ -1,2 +1,12 @@
 ## Initialise database with necessary tables
-library(RSQLite)
+paths <- commandArgs(trailingOnly = TRUE)
+db_path <- as.character(paths[1])
+
+# Initiate an SQLite DB 
+logging_db <- DBI::dbConnect(RSQLite::SQLite(), dbname = db_path)
+
+# Define logging table
+logging_table <- data.frame(BamFilename=character(0), HasBaiFile=integer(0), HasVcfFile=integer(0), ProcessStatus=character(0))
+
+# create logging table in DB
+DBI::dbWriteTable(logging_db, "InputFiles", logging_table)
