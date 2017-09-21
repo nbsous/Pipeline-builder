d9f9733a0fc879cc02886918ae233178510a77f9
adrowe1 <adrowe1@gmail.com>
2017-09-21 11:01:55 +0200

    dev
    not functional yet

--- R-scripts/initSQLiteDB.R
+++ R-scripts/initSQLiteDB.R
@@ -2,11 +2,16 @@
 paths <- commandArgs(trailingOnly = TRUE)
 db_path <- as.character(paths[1])
 
-# Initiate an SQLite DB 
+# Initiate an SQLite DB
 logging_db <- DBI::dbConnect(RSQLite::SQLite(), dbname = db_path)
 
 # Define logging table
-logging_table <- data.frame(BamFilename=character(0), HasBaiFile=integer(0), HasVcfFile=integer(0), ProcessStatus=character(0))
+logging_table <- data.frame(BamFilename=character(0),
+                            HasBaiFile=integer(0),
+                            HasVcfFile=integer(0),
+                            ProcessStatus=character(0)
+                            # add time stamp, platform and fastq columns here .  fix findNewBams.R
+                          )
 
 # create logging table in DB
 DBI::dbWriteTable(logging_db, "InputFiles", logging_table)
